a46db9949837a95a60ce5d537565459754b0e3da
owner <kakuma3090@gmail.com>
2023-10-13 00:57:38 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, } from 'react';
+
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header';
+import HomePage from './components/Pages/HomePage';
+import BlogPage from './components/Pages/BlogPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header /> {/* ヘッダーコンポーネント */}
+
+        <Routes>
+          <Route path="/" element={ <HomePage /> }/> {/*ルート URL ("/") にアクセスしたときに <HomePage /> コンポーネントを表示するためのルーティング設定 */}
+          <Route path="/blog" element={ <BlogPage /> }/>  {/*URL ("/blog") にアクセスしたときに <BlogPage /> コンポーネントを表示するためのルーティング設定 */}
+        </Routes>
+      </div>
+   </Router>
   );
-}
+};
+
 
 export default App;
